ca6b6b129158e7e34029c61a996b7a1d9069d57e
wlatif <wlatif-ext@coreoz.com>
2022-08-07 13:51:14 +0200

    yarn add @react-navigation/native

--- App.tsx
+++ App.tsx
@@ -1,23 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { TailwindProvider } from 'tailwindcss-react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import { TailwindProvider } from "tailwindcss-react-native";
+import Home from "./src/screens/Home";
+
+const NavigationStack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <TailwindProvider>
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-    </TailwindProvider>
+    <NavigationContainer>
+      <TailwindProvider>
+      <NavigationStack.Navigator>
+        <NavigationStack.Screen name="Home" component={Home} />
+      </NavigationStack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Home.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
+  
