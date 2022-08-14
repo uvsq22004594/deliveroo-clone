11ae17f8404cee0ec1feabff087c0a89f9ab0feb
wlatif <wlatif-ext@coreoz.com>
2022-08-14 08:08:39 +0200

    eslint + prettier
    Setup eslint + prettier

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default function Home() {
   return (
@@ -8,13 +17,3 @@ export default function Home() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-  });
-  
